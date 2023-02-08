7634564594154e3d36ddddd833aa2a517fb22312
QuantumXenon <thequantumxenon@gmail.com>
2023-02-08 20:48:46 +0000

    Format files

--- src/main/java/scouttea/em_origins/Entry.java
+++ src/main/java/scouttea/em_origins/Entry.java
@@ -3,7 +3,7 @@ package scouttea.em_origins;
 import net.fabricmc.api.ModInitializer;
 
 public class Entry implements ModInitializer {
-	@Override
-	public void onInitialize() {
-	}
+    @Override
+    public void onInitialize() {
+    }
 }
